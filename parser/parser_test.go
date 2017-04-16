6a3ab64eab5af234017fe4def26af42ff33a385b
Junya Ogasawara <oga.ivc.s27@gmail.com>
2017-04-16 15:30:19 +0900

    Finish implementing Lexer.

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -11,75 +11,80 @@ import (
 )
 
 func TestReturnStatements(t *testing.T) {
-	input := `
-		return 5;
-		return 10;
-		return 993322;
-	`
-	inputStatementCount := 3
-
-	l := lexer.New(input)
-	p := New(l)
-
-	program := p.ParseProgram()
-	checkParserErrors(t, p)
-
-	if program == nil {
-		t.Fatalf("ParseProgram returned nil")
+	tests := []struct {
+		input         string
+		expectedValue interface{}
+	}{
+		{"return 5;", 5},
+		{"return true;", true},
+		{"return x;", "x"},
 	}
 
-	if len(program.Statements) != inputStatementCount {
-		t.Fatalf("program.Statements doesn't contain %d statements. got = %d", len(program.Statements))
-	}
+	for _, tt := range tests {
+		l := lexer.New(tt.input)
+		p := New(l)
 
-	for _, stmt := range program.Statements {
-		returnStmt, ok := stmt.(*ast.ReturnStatement)
+		program := p.ParseProgram()
+		checkParserErrors(t, p)
 
+		if program == nil {
+			t.Fatalf("ParseProgram returned nil")
+		}
+
+		if len(program.Statements) != 1 {
+			t.Fatalf("program.Statements doesn't contain %d statements. got = %d",
+				1, len(program.Statements))
+		}
+
+		stmt, ok := program.Statements[0].(*ast.ReturnStatement)
 		if !ok {
-			t.Errorf("stmt is not *ast.ReturnStatement. got = %s", stmt)
-			continue
+			t.Fatalf("program.Statements[0] isn't ast.ReturnStatement. got=%T", program.Statements[0])
 		}
 
-		if returnStmt.TokenLiteral() != "return" {
-			t.Errorf("returnStmt.TokenLiteral() not 'return', got = %q", returnStmt.TokenLiteral())
+		if !testLiteralExpression(t, stmt.ReturnValue, tt.expectedValue) {
+			return
+		}
+
+		if stmt.TokenLiteral() != "return" {
+			t.Errorf("stmt.TokenLiteral() not 'return', got = %q", stmt.TokenLiteral())
 		}
 	}
-
 }
 
 func TestLetStatements(t *testing.T) {
-	input := `
-	let x = 5;
-	let y = 10;
-	let foobar = 838383;
-	`
-	inputStatementCount := 3
-
-	l := lexer.New(input)
-	p := New(l)
-
-	program := p.ParseProgram()
-	checkParserErrors(t, p)
-
-	if program == nil {
-		t.Fatalf("ParseProgram returned nil")
-	}
-
-	if len(program.Statements) != inputStatementCount {
-		t.Fatalf("program.Statements doesn't contain %d statements. got = %d", len(program.Statements))
-	}
-
 	tests := []struct {
+		input              string
 		expectedIdentifier string
+		expectedValue      interface{}
 	}{
-		{"x"},
-		{"y"},
-		{"foobar"},
+		{"let x = 5;", "x", 5},
+		{"let y = true;", "y", true},
+		{"let foobar = y;", "foobar", "y"},
 	}
 
-	for i, tt := range tests {
-		stmt := program.Statements[i]
+	for _, tt := range tests {
+		l := lexer.New(tt.input)
+		p := New(l)
+
+		program := p.ParseProgram()
+		checkParserErrors(t, p)
+
+		if program == nil {
+			t.Fatalf("ParseProgram returned nil")
+		}
+
+		if len(program.Statements) != 1 {
+			t.Fatalf("program.Statements doesn't contain %d statements. got = %d",
+				1, len(program.Statements))
+		}
+
+		stmt := program.Statements[0]
 		if !testLetStatement(t, stmt, tt.expectedIdentifier) {
+			return
+		}
+
+		val := stmt.(*ast.LetStatement).Value
+		if !testLiteralExpression(t, val, tt.expectedValue) {
 			return
 		}
 	}
--- repl/repl.go
+++ repl/repl.go
@@ -8,7 +8,7 @@ import (
 	"io"
 
 	"github.com/0gajun/monkey/lexer"
-	"github.com/0gajun/monkey/token"
+	"github.com/0gajun/monkey/parser"
 )
 
 const PROMPT = ">> "
@@ -18,15 +18,44 @@ func Start(in io.Reader, out io.Writer) {
 
 	for {
 		fmt.Printf(PROMPT)
-		if !scanner.Scan() {
+		scanned := scanner.Scan()
+		if !scanned {
 			return
 		}
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+const MONKEY_FACE = `            __,__
+   .--.  .-"     "-.  .--.
+  / .. \/  .-. .-.  \/ .. \
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ 0 | 0 /  /   / |
+  \ '- ,\.-"""""""-./, -' /
+   ''-' /_   ^ ^   _\ '-''
+       |  \._   _./  |
+       \   \ '~' /   /
+        '._ '-=-' _.'
+           '-----'
+`
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, MONKEY_FACE)
+	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+	io.WriteString(out, "parser errors: \n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
