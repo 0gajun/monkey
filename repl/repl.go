948b6b89c0dca6071a86dc27b3ad0b9ec9aaeee8
Junya Ogasawara <oga.ivc.s27@gmail.com>
2017-05-23 00:49:30 +0900

    Support Prefix Expressions.

--- repl/repl.go
+++ repl/repl.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/0gajun/monkey/evaluator"
 	"github.com/0gajun/monkey/lexer"
 	"github.com/0gajun/monkey/parser"
 )
@@ -33,8 +34,13 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
+
 	}
 }
 
