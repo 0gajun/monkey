0480a39298434a48c77962938990709e7666b123
Junya Ogasawara <oga.ivc.s27@gmail.com>
2017-05-24 16:00:48 +0900

    Support if expression.

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -21,6 +21,8 @@ func Eval(node ast.Node) object.Object {
 		return evalStatements(node.Statements)
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
+	case *ast.BlockStatement:
+		return evalStatements(node.Statements)
 
 	// Expressions
 	case *ast.IntegerLiteral:
@@ -34,6 +36,8 @@ func Eval(node ast.Node) object.Object {
 		right := Eval(node.Right)
 		left := Eval(node.Left)
 		return evalInfixExpression(node.Operator, left, right)
+	case *ast.IfExpression:
+		return evalIfExpression(node)
 	}
 
 	return nil
@@ -54,6 +58,31 @@ func nativeBoolToBooleanObject(input bool) *object.Boolean {
 		return TRUE
 	} else {
 		return FALSE
+	}
+}
+
+func evalIfExpression(ie *ast.IfExpression) object.Object {
+	condition := Eval(ie.Condition)
+
+	if isTruthy(condition) {
+		return Eval(ie.Consequence)
+	} else if ie.Alternative != nil {
+		return Eval(ie.Alternative)
+	} else {
+		return NULL
+	}
+}
+
+func isTruthy(object object.Object) bool {
+	switch object {
+	case TRUE:
+		return true
+	case FALSE:
+		return false
+	case NULL:
+		return false
+	default:
+		return true
 	}
 }
 
